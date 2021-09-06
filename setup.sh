bf5eec40c99f571e4e069cbafadd9931a9ec9c93
onkar234 <32240917+onkar234@users.noreply.github.com>
2021-09-06 14:50:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"onkartatkare234@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
